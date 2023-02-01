9f36937e93348a715b1f18eb21dc6f9adf7774bf
Luke Sheppard <sheppiegit@gmail.com>
2023-01-31 22:01:55 -0500

    Added a post route to login a user.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
-import { users } from "./data";
+const jwt = require("jsonwebtoken");
+const users = require("./data");
+
+app.use(express.json());
+
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => {
+    return u.username == username && u.password == password;
+  });
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(400).json("username or password incorrect");
+  }
+});
 
 app.listen(3000, () => {
   console.log("Backend server is running");
