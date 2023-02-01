8f7bf6d61c60aaaef3deb3150bfd9556b9387d4e
Luke Sheppard <sheppiegit@gmail.com>
2023-01-31 20:59:54 -0500

    Added mock user data for backend.

---
+++ api/data.js
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    id: "1",
+    username: "luke",
+    password: "luke1234",
+    isAdmin: true,
+  },
+  {
+    id: "2",
+    username: "johnnyboy",
+    password: "johnnyboy123",
+    isAdmin: false,
+  },
+];
--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+import { users } from "./data";
 
 app.listen(3000, () => {
   console.log("Backend server is running");
