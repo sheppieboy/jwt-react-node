8acf90a4b412173e4aca747b99d26d49c1f56653
Luke Sheppard <sheppiegit@gmail.com>
2023-01-31 22:13:01 -0500

    Added a verify function for the jwt token.

--- api/index.js
+++ api/index.js
@@ -30,6 +30,22 @@ app.post("/api/login", (req, res) => {
   }
 });
 
+const verify = (req, res, next) => {
+  const authHeader = req.headers.Authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) {
+        return res.status(401).json("Token is not valid");
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json("you are not authenticated");
+  }
+};
+
 app.listen(3000, () => {
   console.log("Backend server is running");
 });
